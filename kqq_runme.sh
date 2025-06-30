4a8a0d965f9be3ee3b800bd98d94abb2da5fbf47
Qiuqiang Kong <qiuqiangkong@gmail.com>
2025-07-01 00:58:52 +0800

    update

--- kqq_runme.sh
+++
@@ -1,5 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 python train.py --config="./kqq_configs/04a.yaml" --no_log
-
-CUDA_VISIBLE_DEVICES=0 python inference.py \
-	--config="./kqq_configs/04a.yaml" \
-	--ckpt_path="./checkpoints/train/04a/step=2000.pth"
